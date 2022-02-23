b7a0149caa546497b8d492fb9b276a92f449faff
laura aguirre <lauraaguirrenazareth@gmail.com>
2022-02-23 08:07:16 -0500

    taller1.js

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("hola mundo");
+// comentarios en linea 
+/*comentario
+de 
+varias
+lineas
+*/
+
+
+//variables 
+var primerVariable ;
+let edadUsuario ;
+//declarar variables
+var nombreVariable ; 
+//crear variable sin asignar valor 
+/* 
+inicializar variables : 
+asignar un valor al momento de crear la variable 
+*/
+var gradoEstudiante="10A" ;
+/*
+asignar un valor : 
+igualar una variable existente a un nuevo valor 
+*/ 
+edadUsuario=16 ;
+console.log(edadUsuario) ;
+//asignar valor desde otra variable 
+var edad=17 ;
+edadUsuario=edad ;
+console.log(edadUsuario) ;
+/*
+Asignar valor desde el teclado: 
+existen dos maneras de hacerlo, la primera es por ventana emergente y la segunda es por medio de un formulario en html
+*/ 
+var valor=prompt("ingrese un valor:") ;
+console.log("la variable vale " + valor) ;
+/* 
+constantes: son valores que no pueden ser cambiados durante la ejecucion del codigo. las constantes siempre deben ser 
+*/
+const PI=3.1416 ;
+//imprimir por consola
+//simple
+console.log("") ;
+console.log(valor) ;
+//concatenado
+console.log("" + valor) ;
---
+++ taller1.js
@@ -0,0 +1,15 @@
+//archivo de practicas y pruebas de JavaScript 
+/* 
+taller 1 
+fecha 23/02/2022
+realizado por: laura aguirre y mariana holguin 
+*/
+var nombreEstudiante ; // "string" 
+var gradoEstudiante= "10A"; // "string" 
+var edadEstudiante ; // "int"
+nombreEstudiante= "lamar"
+edadEstudiante= prompt ("ingrese un valor") ;
+console.log("el estudiante"+nombreEstudiante+"del grado"+gradoEstudiante+ "tiene"+edadEstudiante) ; 
+
+
+
