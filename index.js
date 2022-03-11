c8a4fd41d7095e14b1b20ed00c0cfb5eace583f0
laura aguirre <lauraaguirrenazareth@gmail.com>
2022-03-11 11:31:23 -0500

    solucion taller 2

--- index.js
+++ index.js
@@ -44,3 +44,78 @@ console.log("") ;
 console.log(valor) ;
 //concatenado
 console.log("" + valor) ;
+//operadores 
+//sumar 
+var suma ; 
+suma=5 + 4 ;
+console.log (suma) ; 
+var a = 12; 
+suma = a + 8 ;
+console.log (suma) ; 
+var b = 20;
+suma = a + b 
+console.log (suma) ;
+//resta 
+var resultado ;
+resultado = a - b ;
+console.log(resultado) ;
+resultado = b - a ;
+console.log(resultado) ;
+//multiplicacion 
+var producto ;
+producto = a * b ;
+console.log(producto) ;
+//division 
+var division ;
+division = a/b ;
+console.log(division) ;
+division = b/a ;
+console.log(division) ;
+//orden de operaciones 
+var result
+result = a * b ;
+console.log(result) ;
+result = a * b - 10 ;
+console.log(result) ;
+result = a *(b - 10) ;
+console.log(result) ;
+//exponente/potencia
+var exp = 3**2 ;
+console.log(exp) ;
+exp = 3**3 ;
+console.log(exp) ;
+//cadenas o string
+var cadenita1 ="3" ;
+var cadenita2 ="5" ;
+var sumacadenenas =cadenita1 + cadenita2 ;
+console.log(sumacadenenas) ;
+var nombresito ="Laura" ;
+var apedillito ="aguirre" ;
+var nomCompleto = nombresito + " " + apedillito ;
+console.log(nomCompleto) ;
+var longitud =nombresito.length ;
+console.log(longitud) ;
+console.log(nomCompleto.length) ;
+//encontrar caracteres dada la posicion en el vector 
+var primeraLetra ;
+primeraLetra =nombresito[0] ;
+console.log(primeraLetra) ;
+console.log(nombresito[1]) ;
+//encontrar la ultima posicion de una cadena o vector 
+nombresito[nombresito.length-1] ;
+console.log(nombresito[nombresito.length-1])  ;
+//se debe buscar la longitud de la cadena y restarle 1 para encontrar la ultima letra 
+//valores inmuntables
+//las posiciones de una cadena se pueden consultar pero no se pueden reasignar su valor 
+nombresito[0]= "p" ;
+console.log(nombresito) ;
+nombresito= "mariana" ;
+console.log(nombresito) ;
+
+
+
+
+
+
+
+
---
+++ taller2.js
@@ -0,0 +1,92 @@
+//taller 2 
+console.log("solución punto #1") ;
+var areaCuadrado
+var volumenCubo
+var ladoC = prompt("por favor ingrese el lado del cuadrado") ;
+areaCuadrado = ladoC*ladoC ;
+console.log("el area del cuadrado es " + areaCuadrado) ;
+volumenCubo = ladoC**3
+console.log("el volumen del cubo es" + volumenCubo) ;
+
+
+
+console.log("solución punto #2") ;
+var base 
+var ladoR 
+base = prompt("por favor ingrese la base del rectangulo") ;
+ladoR = prompt("por favor ingrese el lado del rectangulo") ;
+var areaRectangulo 
+areaRectangulo = base*ladoR
+console.log("el area del rectangulo es" + areaRectangulo) ;
+var perimetroRectangulo 
+perimetroRectangulo = ladoR+ladoR +base+base //2(ladoR+base)
+console.log("el perimetro del rectangulo es" + perimetroRectangulo) ;
+
+
+
+console.log("solución punto #3") ;
+var areaCirculo
+var volumenEsfera
+var diametro = prompt("por favor ingrese el diametro") ;
+diametro = 2*Math.PI
+areaCirculo = prompt("por favor ingrese el area del circulo") ;
+volumenEsfera = prompt("por favor ingrese el volumen de la esfera") ;
+areaCirculo = Math.PI*(diametro/2)**2
+volumenEsfera = 4/3*Math.PI*(diametro/2)**3
+console.log("el area del circulo es" + areaCirculo) ;
+console.log("el volumen de la esfera es" + volumenEsfera) ;
+
+
+
+console.log("solución punto #4") ;
+var base1 = prompt("por favor ingrese la base1") ;
+var base2 = prompt("por favor ingrese la base2") ;
+var h = prompt("por favor ingrese la altura") ;
+var areaT
+areaT = 1/2*(base1+base2)*h
+console.log("el area del trapezoide es" + areaT) ;
+
+
+
+console.log("solución punto #5") ;
+var ancho = prompt("por favor ingrese el ancho") ;
+var largo = prompt("por favor ingrese el largo") ;
+var profundidad = prompt("por favor ingrese la profundidad") ;
+var volumenP = largo*ancho*profundidad
+console.log("la piscina se llena con " + volumenP + " litros de agua" ) ;
+
+
+
+console.log("solución punto #6") ;
+var alto = prompt("por favor ingrese el alto") ;
+var ancho = prompt("por favor ingrese el ancho") ;
+var baseP = prompt("por favor ingrese la base");
+var altura = prompt("por favor ingrese la altura") ;
+var areaPared = baseP*altura 
+var totalPago = 22000*areaPared
+console.log("el total a pagar al pintor es" + totalPago) ;
+
+
+
+console.log("solución punto #7") ;
+//punto a 
+var longitudCenti = prompt("por favor ingrese los centimetros que desea convertir") ;
+var kilometros = longitudCenti*(1/100)*(1/1000) ;
+console.log("la equivalencia de los centimetros a kilometros es" + kilometros) ;
+
+//punto b 
+var volumenDeci = prompt("por favor ingrese los decilitros que desea convertir") ;
+var litro = volumenDeci*(0.1/1.0) ;
+console.log("la equivalencia de los decilitros a litros es" + litro) ;
+
+//punto c
+var masaMicro = prompt("por favor ingrese los microgramos que desea convertir") ;
+var gramos = masaMicro*(1.0/1000000) ;
+console.log("la equivalencia de los microgramos a gramos es" + gramos) ;
+
+//punto d 
+var distanciaKm = prompt("por favor ingrese los kilometros que desea convertir") ;
+var milimetros = (1000/1)*(1000/1) ;
+console.log(" la equivalencia de los kilometros a milimetros es" + milimetros) ; 
+
+
