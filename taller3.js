3e10efbbf96a9a4381c32ddc6c9e3b5271af6544
laura aguirre <lauraaguirrenazareth@gmail.com>
2022-03-23 08:07:44 -0500

    taller3.js

---
+++ taller3.js
@@ -0,0 +1,45 @@
+//punto 1 
+var litros ;
+var tiempo ;
+litros = prompt ("ingrese una cantidad en litros ") ;
+tiempo = litros * 1/145 ;
+console.log("la piscina tarda en llenarse" + tiempo + "horas") ;
+
+
+
+//punto 2
+var metros = prompt("por favor ingrese los metros que desea covertir") ;
+var milimetros = metros * (1000/1) ;
+console.log("la equivalencia de los metros a milimetros es " + milimetros) ;
+
+
+
+//punto 3 
+var decimetros = milimetros * (1/100) ;
+console.log("la equivalencia de los milimetros a decimetros es " + decimetros) ;
+
+
+
+//punto 4 
+var kilometros = decimetros * (1/10000) ;
+console.log("la equivalencia de los decimetros a kilometros es " + kilometros) ;
+
+
+
+//punto 5
+var horas ;
+var dinero ;
+horas = prompt ("por favor ingrese la cantidad de horas") ;
+dinero = horas * 60 * 120 ;
+console.log("la cantidad de dinero que debe pagar es " + dinero) ;
+
+
+
+//punto 6 
+var horas
+var costo
+horas = prompt("ingrese la cantidad de horas que desea ver")
+costo =  (horas - 1 ) * (25000) + 40000
+console.log("la cantidad de horas que debe pagar es " + costo) 
+
+
