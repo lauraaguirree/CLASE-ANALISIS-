db46320b95dea0a98047dd0783f2fe8cc295e0d3
laura <lauraaguirrenazareth@gmail.com>
2022-05-04 06:53:35 -0500

    evaluacion

---
+++ EvaluacionCondicionales.js
@@ -0,0 +1,69 @@
+//Laura y Mariana
+
+//punto 1
+var ValorVariable= "2";
+ 
+if (ValorVariable != 6 ){
+   console.log("La condición es verdadera")
+}
+
+//punto 2 
+var valor= "6" ;
+ 
+if (valor == 6 && valor <= 10){
+   console.log("El valor es correcto")
+}
+
+//punto 3 
+
+var numero= "30" ;
+ 
+if (numero=== "2"){
+   console.log('El número es igual que "2" en valor y tipo');
+}else if(numero==="3"){
+    console.log('El número es igual a "3" en tipo y valor');
+}else if(numero>5 && numero <=15){
+    console.log("El número es mayor que 5 y menor igual que 15");
+}else if(numero>15){
+    console.log("El número es mayor que 15");
+}
+
+
+//Respuesta 3.A
+    numero= "2"
+//Respuesta 3.B
+    numero= "3"
+//Respuesta 3.C
+    numero= 10
+//Respuesta 3.D
+    numero= 30
+
+//punto 4 
+var n1 = "2" ;
+var n2 = "5" ;
+var simbolo = "+" ;
+
+if (simbolo= "+")  {
+    console.log (n1 + n2) ; 
+}else if (simbolo = "-") {
+ console.log (n1-n2) ;
+ }else if (simbolo = "*") {
+ console.log (n1*n2) ;
+ }
+
+ //punto 5
+ var pais = "alemania" ;
+if (pais == "alemania") {
+    console.log ("su capital es berlin") ;
+    }else if( pais == "brasil") {
+console.log ("su capital es brasilia ")
+}else if( pais == "canada") {
+  console.log ("su capital es ottawa")
+}else if( pais == "colombia") {
+  console.log ("su capital es bogota")
+   } else {
+ console.log ("la base de datos esta en construccion") ;
+}
+
+
+
